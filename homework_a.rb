aea99bcb2d2340a3c7f6bbb4681bb93e3f944a73
Stephen Ramsay <rabrandom@gmail.com>
2019-11-25 14:38:58 +0000

    completed getting methods for part a

---
+++ homework_a.rb
@@ -0,0 +1,17 @@
+class CodeClanStudent
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+
+  def get_student_name
+    return @name
+  end
+
+  def get_student_cohort
+    return @cohort
+  end
+
+
+end
--- specs/homework_a_specs.rb
+++ specs/homework_a_specs.rb
@@ -4,6 +4,20 @@ require_relative('../homework_a.rb')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
-class TestBankAccount < MiniTest::Test
+class TestCodeClanStudent < MiniTest::Test
+
+def test_get_student_name
+  student = CodeClanStudent.new("Stephen", 36)
+  assert_equal("Stephen", student.get_student_name)
+end
+
+
+def test_get_student_cohort
+  student = CodeClanStudent.new("Stephen", 36)
+  assert_equal(36, student.get_student_cohort)
+end
+
+
+
 
 end
