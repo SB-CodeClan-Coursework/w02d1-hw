3882af6ad545cef7d4243dd7e390d3bc0cf86f73
Stephen Ramsay <rabrandom@gmail.com>
2019-11-25 15:47:49 +0000

    refactor

--- homework_b.rb
+++ homework_b.rb
@@ -1,25 +1,29 @@
 class SportsTeam
+
+attr_accessor :coach, :players, :team_name
+# attr_reader
+
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
   end
 
-  def get_players
-    return @players
-  end
-
-  def get_team_name
-    return @team_name
-  end
-
-  def get_coach
-    return @coach
-  end
-
-  def set_coach_name(coach)
-    @coach = coach
-  end
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_team_name
+  #   return @team_name
+  # end
+  #
+  # def get_coach
+  #   return @coach
+  # end
+  #
+  # def set_coach_name(coach)
+  #   @coach = coach
+  # end
 
 
 
--- specs/homework_b_specs.rb
+++ specs/homework_b_specs.rb
@@ -6,29 +6,47 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSportsTeam < MiniTest::Test
 
-def test_get_players
-  sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
-  assert_equal(["Robert", "Shane", "Robert M"], sports_team.get_players)
-end
+  def test_get_players
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+    assert_equal(["Robert", "Shane", "Robert M"], sports_team.players)
+  end
 
-def test_get_team_name
-  sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
-  assert_equal("E37 Table Tennis Team", sports_team.get_team_name)
-end
+  def test_get_team_name
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+    assert_equal("E37 Table Tennis Team", sports_team.team_name)
+  end
 
-def test_get_coach
-  sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
-  assert_equal("Big Bob", sports_team.get_coach)
-end
+  def test_get_coach
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+    assert_equal("Big Bob", sports_team.coach)
+  end
 
-def test_set_coach_name
-  sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
-  sports_team.set_coach_name("Egg McGee")
-  assert_equal("Egg McGee", sports_team.get_coach)
-end
+  def test_set_coach_name
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+    sports_team.coach = "Egg McGee"
+    assert_equal("Egg McGee", sports_team.coach)
+  end
 
-
-
+# def test_get_players
+#   sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+#   assert_equal(["Robert", "Shane", "Robert M"], sports_team.get_players)
+# end
+#
+# def test_get_team_name
+#   sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+#   assert_equal("E37 Table Tennis Team", sports_team.get_team_name)
+# end
+#
+# def test_get_coach
+#   sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+#   assert_equal("Big Bob", sports_team.get_coach)
+# end
+#
+# def test_set_coach_name
+#   sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
+#   sports_team.set_coach_name("Egg McGee")
+#   assert_equal("Egg McGee", sports_team.get_coach)
+# end
 
 
 
