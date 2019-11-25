b6a9638e363757a7fda79426140f96b251a7f926
Stephen Ramsay <rabrandom@gmail.com>
2019-11-25 17:16:17 +0000

    did more of part b

--- homework_b.rb
+++ homework_b.rb
@@ -1,17 +1,35 @@
 class SportsTeam
 
 attr_accessor :coach, :players
-attr_reader :team_name
+attr_reader :team_name, :points
 
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
   end
 
-  def new_player(bacon)
-    @players << bacon
+  def new_player(name)
+    @players << name
   end
+
+  def check(name_of_new_player)
+    for player in @players
+      if player == name_of_new_player
+        return true
+      end
+    end
+    return false
+  end
+
+  def add_points(did_they_win)
+    if did_they_win = "win"
+      @points += 1
+    # else
+    # += 0
+  end
+    end
 
 
   # def get_players
--- specs/homework_b_specs.rb
+++ specs/homework_b_specs.rb
@@ -33,6 +33,23 @@ class TestSportsTeam < MiniTest::Test
     assert_equal(["Robert", "Shane", "Robert M", "Bacon McGee"], sports_team.players)
   end
 
+  def test_check_is_player_exists_true
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M", "Bacon McGee"], "Big Bob")
+    assert_equal(true, sports_team.check("Shane"))
+  end
+
+  def test_check_is_player_exists_false
+    sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M", "Bacon McGee"], "Big Bob")
+    assert_equal(false, sports_team.check("Sausage McGee"))
+  end
+
+  def test_add_points
+  sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M", "Bacon McGee"], "Big Bob")
+  sports_team.add_points("win")
+  assert_equal(1, sports_team.points)
+  end
+
+
 # def test_get_players
 #   sports_team = SportsTeam.new("E37 Table Tennis Team", ["Robert", "Shane", "Robert M"], "Big Bob")
 #   assert_equal(["Robert", "Shane", "Robert M"], sports_team.get_players)
